de731e225b4683c9a81ad104349e517a25b73145
John Ketiku <144270920+John-Ketiku-1@users.noreply.github.com>
2025-01-25 01:32:34 +0000

    quick fixes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use('/dist', express.static(path.join(__dirname, '/dist')));
 app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 app.get('/', function(req,res){
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/docs/index.html");
 })
 
 app.listen(port, function(){
